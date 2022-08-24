5ae84f10fbf3c2ed81288d9020ecaee96cff13cd
sapphirehead <djey36@gmail.com>
2022-08-25 00:21:04 +0300

    flake8 fourth

--- backend/api/mixins.py
+++ backend/api/mixins.py
@@ -1,10 +1,10 @@
 from django.shortcuts import get_object_or_404
 
-from .serializers import FavoritesSerializer
 from recipes.models import Recipe
 from rest_framework.response import Response
 from rest_framework.status import (HTTP_201_CREATED, HTTP_204_NO_CONTENT,
                                    HTTP_400_BAD_REQUEST)
+from .serializers import FavoritesSerializer
 
 
 class AddDelMixin:
